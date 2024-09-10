84d197bcddc9000682374b5b58fed170e8e5fee8
fonsoccs <alfonso.licir@gmail.com>
2024-09-09 20:24:43 -0500

    feat:adding-query-for-report2

--- sql_queries/querys.sql
+++ sql_queries/querys.sql
@@ -26,3 +26,32 @@ from job_list
 group by department, job
 order by department, job;
 
+
+-- report 2: List of ids, name and number of employees hired of each department 
+-- that hired more employees than the mean of employees hired in 2021 for all the departments, 
+-- ordered by the number of employees hired (descending).
+-- Number of employees hired for each job and department
+
+with hired_employees_by_department as (
+    select 
+        hired_employees.department_id, 
+        departments.department as department,
+        count(hired_employees.id) as number_of_employees
+    from hired_employees
+    left join departments on hired_employees.department_id = departments.id
+    where extract(year from hired_employees.datetime) = 2021
+    and name is not null and department_id is not null and job_id is not null
+    group by hired_employees.department_id, departments.department
+),
+mean_hired_employee as (
+    select avg(number_of_employees) as mean_hired_employees
+    from hired_employees_by_department
+)
+select
+    department_id as id,
+    department,
+    number_of_employees as hired
+from hired_employees_by_department
+join mean_hired_employee on true
+where number_of_employees > mean_hired_employees
+order by hired_employees_by_department.number_of_employees desc;
