0dca05d8f95a0c08d0130052aad27174f1f6b42e
fonsoccs <alfonso.licir@gmail.com>
2024-09-12 11:29:52 -0400

    feat:modified-endpoint-to-use-config-file

---
+++ app/config.py
@@ -0,0 +1,21 @@
+# this configure the information of the entities used by the endpoints
+entity_config = {
+    "departments": {
+        "table_name": "departments",
+        "columns": ["id", "department"],
+        "filename": "departments.csv",
+        "file_regex": r'^departments\.csv$'
+    },
+    "jobs": {
+        "table_name": "jobs",
+        "columns": ["id", "job"],
+        "filename": "jobs.csv",
+        "file_regex": r'^jobs\.csv$'
+    },
+    "employees": {
+        "table_name": "hired_employees",
+        "columns": ["id", "name", "datetime", "department_id", "job_id"],
+        "filename": "hired_employees.csv",
+        "file_regex": r'^hired_employees\.csv$'
+    },
+}
--- app/routers/upload.py
+++ app/routers/upload.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 from io import BytesIO
 from fastapi import APIRouter, UploadFile, File
+from app.config import entity_config
 
 router = APIRouter()
 
@@ -22,8 +23,8 @@ def process_data(data: pd.DataFrame, entity: str):
     # TODO: Implement logic for data insertion, improve documentation
     pass
 
-@router.post("/uploadcsv/")
-async def post_upload_file(file: UploadFile):
+@router.post("/{entity_name}/uploadcsv")
+async def post_upload_file(file: UploadFile, entity_name: str):
     """
     Uploads a file and validates the name and the structure of the content (columns).
     If the file is valid, the data is processed and inserted into the database.
@@ -35,30 +36,31 @@ async def post_upload_file(file: UploadFile):
         dict: A dictionary containing either an error message or a success message.
     """
 
+    if entity_name in entity_config:
+        entity = entity_config[entity_name]
+    else:
+        return {"error": "Entity not found"}
+
     file_pth = file.filename
 
     # validate the file received
-    if not re.match(r'^(departments|jobs|hired_employees)\.csv$', file_pth):
-        return {"error": "Allowed files are: departments.csv, jobs.csv, or hired_employees.csv"}
+    if not re.match(entity['file_regex'], file_pth):
+        return {"error": f"The allowed file for entity {entity['filename']} is: {entity['filename']}"}
 
     # prepare the expected columns to validate file structure
-    if re.match(r'^departments\.csv$', file_pth):
-        expected_columns = {'id', 'department'}
-    elif re.match(r'^jobs\.csv$', file_pth):
-        expected_columns = {'id', 'job'}
-    else: #hired_employees.csv
-        expected_columns = {'id', 'name', 'datetime', 'department_id', 'job_id'}
+    expected_columns = entity['columns']
+    # print(expected_columns)
 
     # read the file contents to validate the columns
     with open(file_pth, "wb") as F:
         contents = await file.read()
         df = pd.read_csv(BytesIO(contents))
-        print(df.columns.tolist())
 
-    if set(df.columns.tolist()) != expected_columns:
+    print(df.columns.tolist())
+    if df.columns.tolist() != expected_columns:
         return {"error": "Unexpected columns in the uploaded file"}
 
     # execute data insertion
-    process_data(df, file_pth.replace('.csv', ''))
+    process_data(df, entity['table_name'])
 
     return {"message": "File upload successful"}
